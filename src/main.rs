b191fb784f903cfb4df891b38e5ad3c26d46bf11
Nick Ginther <nginth@gmail.com>
2016-02-25 13:17:20 -0600

    basic response working

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,27 @@
 use std::net::{TcpStream, TcpListener, SocketAddrV4, Ipv4Addr};
-use std::io::{BufReader, BufRead, BufWriter, Read};
+use std::io::{BufReader, BufRead, BufWriter, Write};
 use std::str;
 
 fn handle_request(request: &TcpStream) {
     let mut stream = BufReader::with_capacity(1024, request);
-    println!("here");
     
     let len = {
+        // grab 1024 bytes from the stream
         let buf = stream.fill_buf().unwrap();
-        println!("not here");
         let http_req = match str::from_utf8(&buf) {
             Ok(string) => string,
             Err(e) => panic!("cannot convert buf into utf8: {}", e),
         };
+
         println!("{}", http_req);
         buf.len()
     };
-    
+    // let the BufReader know not to read those bytes again
     stream.consume(len);
+
+    // send response
+    let mut stream = BufWriter::new(request);
+    stream.write(b"hello").unwrap();
 }  
 
 fn main() {
