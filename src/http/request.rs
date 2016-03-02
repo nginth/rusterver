a4a6d690a06ffe97fb87263d8b723b9726994359
Nick Ginther <nginth@gmail.com>
2016-03-02 02:05:27 -0600

    begin work on parsing http request

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,3 +1,7 @@
+use std::string::String;
+use std::io::{Lines, BufReader};
+use std::net::TcpStream;
+
 pub struct HttpRequest<'a> {
     method: &'a str,
     path: &'a str,
@@ -14,4 +18,24 @@ impl<'a> HttpRequest<'a> {
         HttpRequest 
         { method: m, path: p, version: v}
     }
+
+    /* TODO: handle request bodies */
+    pub fn new(lines: Lines<BufReader<&TcpStream>>) -> HttpRequest<'a> { 
+        let mut req = HttpRequest {method: "x", path: "y", version: "z"};
+        //figure out how lines.next() works
+
+        for line in lines {
+            let line_string = line.unwrap();
+            let slice = &line_string[..];
+            /* if slice is empty, we've hit the end of the request (unless there's a body) */
+            if slice == "" { break; };
+            parse_line(slice, &mut req);
+        }
+
+        req
+    }
+}
+
+
+fn parse_line(slice: &str, mut request: &mut HttpRequest) {
 }
