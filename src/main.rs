3c454123d2f075abf6debd09df5fec30772d15b4
Nick Ginther <nginth@gmail.com>
2016-03-02 02:04:56 -0600

    refactor into separate functions

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,39 @@
+/* rusterver - a basic memory safe multithreaded webserver
+ * While it is memory safe, it is completely unsecure, so beware
+ * of actually using it as a web server.
+ */
 use std::net::{TcpStream, TcpListener, SocketAddrV4, Ipv4Addr};
 use std::io::{BufReader, BufRead, BufWriter, Write};
 use std::str;
+use std::string::String;
 use std::thread;
 
 mod http;
 use http::request::HttpRequest;
 use http::response::HttpResponse;
 
-fn handle_request(request: &TcpStream) {
-    let mut stream = BufReader::with_capacity(1024, request);
-    
-    // block in order to borrow stream again for consume()
-    let len = {
-        // grab 1024 bytes from the stream
-        let buf = stream.fill_buf().unwrap();
-        let http_req = match str::from_utf8(&buf) {
-            Ok(string) => string,
-            Err(e) => panic!("cannot convert buf into utf8: {}", e),
-        };
+fn do_response(stream: &TcpStream) {
+    let http_request: HttpRequest = parse_request(stream);
+    send_response(stream);
+} 
 
-        println!("{}", http_req);
-        buf.len()
-    };
-    // let the BufReader know not to read those bytes again
-    stream.consume(len);
+fn parse_request(stream: &TcpStream) -> HttpRequest {
+    const CRLF: &'static str = "\r\n";
+    let mut reader = BufReader::new(stream);
+    let mut request_string = String::new();
 
+    HttpRequest::new(reader.lines())
+} 
+
+fn send_response(stream: &TcpStream) {
     // send response
-    let mut stream = BufWriter::new(request);
-    let mut response = HttpResponse::new("200", "OK");
+    let mut writer = BufWriter::new(stream);
+    let response = HttpResponse::new("200", "OK");
     let mut buf = Vec::new();
     let byte_response: &[u8] = response.get_byte_response(&mut buf);
 
-    stream.write(byte_response).unwrap();
-}  
+    writer.write(byte_response).unwrap();
+}
 
 fn main() {
     let port = 8080;
@@ -41,11 +42,11 @@ fn main() {
     let listener = TcpListener::bind(socket_addr).unwrap();
 
     println!("Server opened on port {}", socket_addr);
-    for request in listener.incoming() {
-        match request {
-            Ok(request) => {
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
                 thread::spawn(move || {
-                    handle_request(&request)
+                    do_response(&stream)
                 });
             },
             Err(e)      => println!("{}", e),
